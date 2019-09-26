caf00d8127ab83c2cedc33f36e818c62f0b78d28
DevTony101 <tonycortesano@gmail.com>
2019-09-26 14:11:14 -0500

    Succesful creation of a profesor. Adding the sortable tables and the layout table for the ajax call

--- src/main/java/edu/unimagdalena/springacademic/controllers/ProfesorController.java
+++ src/main/java/edu/unimagdalena/springacademic/controllers/ProfesorController.java
@@ -1,7 +1,16 @@
 package edu.unimagdalena.springacademic.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import edu.unimagdalena.springacademic.entities.Profesor;
+import edu.unimagdalena.springacademic.services.ProfesorService;
 
 /**
  * ProfesorController
@@ -9,9 +18,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProfesorController {
 
+  @Autowired
+  private ProfesorService service;
+
   @GetMapping("/profesorado")
-  public String profesorado() {
+  public String profesorado(Model model) {
+    model.addAttribute("profesor", new Profesor());
     return "busquedaProfesorado";
   }
-  
+
+  @PostMapping("/crearProfesor")
+  public String crearProfesor(@ModelAttribute @Valid Profesor profesor, Model model) {
+    service.guardarProfesor(profesor);
+    model.addAttribute("profesor", new Profesor());
+    return "busquedaProfesorado";
+  }
+
 }
---
+++ src/main/resources/static/js/adminBusquedaProf.js
@@ -0,0 +1,5 @@
+function setup() {
+  noCanvas();
+  // $('#tbBusqueda').DataTable();
+  
+}
