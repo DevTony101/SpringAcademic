1efac7e0493f24f31955751cda9c7c3c44f683f7
DevTony101 <tonycortesano@gmail.com>
2019-11-02 15:06:00 -0500

    Adding PUT endpoint

--- src/main/java/edu/unimagdalena/springacademic/controllers/rest/RAlumnoController.java
+++ src/main/java/edu/unimagdalena/springacademic/controllers/rest/RAlumnoController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,7 +41,12 @@ public class RAlumnoController {
     } else {
       alumno.setFechaBaja(null);
     }
-    aService.actualizarAlumno(alumno);
+    aService.actualizarAlumno(alumno, false);
+  }
+
+  @PutMapping("/alumnos")
+  public Alumno actualizarAlumno(@RequestBody Alumno alumno) {
+    return aService.actualizarAlumno(alumno, true);
   }
 
 }
