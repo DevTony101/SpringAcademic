a1c8b864aab98821e5fed015590af1849b477b79
DevTony101 <tonycortesano@gmail.com>
2019-11-14 11:59:34 -0500

    Adding utils folder

---
+++ src/main/java/edu/unimagdalena/springacademic/utils/Constants.java
@@ -0,0 +1,11 @@
+package edu.unimagdalena.springacademic.utils;
+
+/**
+ * Constants
+ */
+public class Constants {
+
+  public static final String GMAIL_USER = "app.plus.sas@gmail.com";
+  public static final String GMAIL_PASSWORD = "softwareltda";
+  
+}
